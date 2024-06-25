5b2c9b06736e305c9b7b3fc41ba9b5b78d64da4d
hafnerda <163887163+hafnerda@users.noreply.github.com>
2024-06-25 13:51:45 +0200

    Önderung, dass die LED an udn abschwillt.

--- esr2024_g05_msp430pulseconverter/msp430fr235x_adc12_21.c
+++ esr2024_g05_msp430pulseconverter/msp430fr235x_adc12_21.c
@@ -1,132 +1,72 @@
-/* --COPYRIGHT--,BSD_EX
- * Copyright (c) 2016, Texas Instruments Incorporated
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * *  Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * *  Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * *  Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *******************************************************************************
- *
- *                       MSP430 CODE EXAMPLE DISCLAIMER
- *
- * MSP430 code examples are self-contained low-level programs that typically
- * demonstrate a single peripheral function or device feature in a highly
- * concise manner. For this the code may rely on the device's power-on default
- * register values and settings such as the clock configuration and care must
- * be taken when combining code from several examples to avoid potential side
- * effects. Also see www.ti.com/grace for a GUI- and www.ti.com/msp430ware
- * for an API functional library-approach to peripheral configuration.
- *
- * --/COPYRIGHT--*/
-//******************************************************************************
-//  MSP430FR235x Demo - ADC, Window Comparator, 2.5V ref, Timer trigger, DCO
-//
-//  Description: This example works on Repeat-Single-Channel Mode.
-//  A2 is sampled in every 0.5s using the TB1.1 trigger.
-//  Window comparator is used to generate interrupts to indicate when the input
-//  voltage goes above the High_Threshold (LED blink fast) or below the Low_Threshold
-//  (LED blink slow) or in-between the high and low thresholds (LED off). Timer_B0 is
-//  used as an interval timer to control the LED at P1.0 to toggle fast/slow or turn off
-//  according to the ADC Hi/Lo/In interrupts.
-//  ACLK = XT1 = 32768Hz, MCLK = SMCLK = default DCODIV ~1MHz.
-//
-//               MSP430FR2355
-//            -----------------
-//        /|\|              XIN|-
-//         | |                 | 32kHz
-//         --|RST          XOUT|-
-//           |                 |
-//       >---|P1.2/A2     P1.0 |--> LED
-//
-//
-//   LX. Chen
-//   Texas Instruments Inc.
-//   June 2020
-//   Built with IAR Embedded Workbench v7.12.4 & Code Composer Studio v9.3
-//******************************************************************************
 #include <msp430.h>
 
-#define    High_Threshold    3931                        //~2.4V
-#define    Low_Threshold     2789                        //~1.7V
+#define High_Threshold 3931 // ~2.4V
+#define Low_Threshold 2789  // ~1.7V
 
-unsigned int SlowToggle_Period = 20000-1;
-unsigned int FastToggle_Period = 3000-1;
+unsigned int adcResult;              // Temporarily stores the ADC value
+volatile unsigned int pwm_duty_cycle = 0; // PWM duty cycle
+volatile int pwm_direction = 0;           // Direction of PWM change (1 = increase, -1 = decrease)
 
-unsigned int adcResult;                     // Temporarily stores the ADC value
-volatile unsigned int low_threshold_crossed = 0;  // State variable
-volatile unsigned int high_threshold_crossed = 0; // State variable for high threshold
+void configurePWM(void) {
+    // Configure Timer0 for PWM
+    TB0CCR0 = 1023;                      // PWM Period
+    TB0CCTL1 = OUTMOD_7;                 // CCR1 reset/set
+    TB0CCR1 = pwm_duty_cycle;            // CCR1 PWM duty cycle
+    TB0CTL = TBSSEL_1 | MC_1 | TBCLR;    // ACLK, up mode, clear TBR
+}
 
-int main(void)
-{
-    WDTCTL = WDTPW | WDTHOLD;               // Stop WDT
-
-    P1DIR |= BIT0;                          // Set P1.0 output direction
-    P1OUT &= ~BIT0;                         // Clear the P1.0 output
-
-    // Configure ADC A2 pin
-    P1SEL0 |= BIT2;
-    P1SEL1 |= BIT2;
-
-    // Disable the GPIO power-on default high-impedance mode to activate
-    // previously configured port settings
-    PM5CTL0 &= ~LOCKLPM5;
-
-    // Configure XT1 oscillator
-    P2SEL1 |= BIT6 | BIT7;                              // P2.6~P2.7: crystal pins
-    CSCTL4 = SELA__XT1CLK;                              // Set ACLK = XT1; MCLK = SMCLK = DCO
-    do
-    {
-        CSCTL7 &= ~(XT1OFFG | DCOFFG);                  // Clear XT1 and DCO fault flag
-        SFRIFG1 &= ~OFIFG;
-    } while (SFRIFG1 & OFIFG);                          // Test oscillator fault flag
-
-    // Configure ADC;
+void configureADC(void) {
+    // Configure ADC
     ADCCTL0 = ADCSHT_2 | ADCON;                         // sample-and-hold 16 ADCCLK cycles, ADCON
-    ADCCTL1 = ADCSHP | ADCSHS_2 | ADCSSEL_1 | ADCCONSEQ_2;     // TB1.1 trigger; ACLK for ADCCLK; Rpt single ch
+    ADCCTL1 = ADCSHP | ADCSHS_2 | ADCSSEL_1 | ADCCONSEQ_2; // TB1.1 trigger; ACLK for ADCCLK; Rpt single ch
     ADCCTL2 = ADCRES_2;                                 // 12-bit conversion results
     ADCMCTL0 = ADCINCH_2 | ADCSREF_1;                   // Vref 2.5v, A2
     ADCHI = High_Threshold;                             // Window Comparator Hi-threshold
     ADCLO = Low_Threshold;                              // Window Comparator Lo-threshold
     ADCIE |= ADCHIIE | ADCLOIE | ADCINIE;               // Enable ADC conv complete interrupt
+}
 
+void configureClocks(void) {
+    // Configure XT1 oscillator
+    P2SEL1 |= BIT6 | BIT7;                              // P2.6~P2.7: crystal pins
+    CSCTL4 = SELA__XT1CLK;                              // Set ACLK = XT1; MCLK = SMCLK = DCO
+    do {
+        CSCTL7 &= ~(XT1OFFG | DCOFFG);                  // Clear XT1 and DCO fault flag
+        SFRIFG1 &= ~OFIFG;
+    } while (SFRIFG1 & OFIFG);                          // Test oscillator fault flag
+}
+
+void configureTimer1(void) {
+    // Configure ADC timer trigger TB1.1
+    TB1CCR0 = 8192 - 1;                                 // PWM Period, reduce the period for faster sampling
+    TB1CCR1 = 4096 - 1;                                 // Duty cycle TB1.1
+    TB1CCTL1 = OUTMOD_3;                                // TB1CCR1 set/reset mode
+    TB1CTL = TBSSEL_1 | MC_1 | TBCLR;                   // ACLK, up mode
+    TB1CCTL0 = CCIE;                                    // Enable Timer1 CCR0 interrupt
+}
+
+void configureReference(void) {
     // Configure Internal reference voltage
     PMMCTL0_H = PMMPW_H;                                // Unlock the PMM registers
     PMMCTL2 |= INTREFEN | REFVSEL_2;                    // Enable internal 2.5V reference
     __delay_cycles(400);                                // Delay for reference settling
+}
 
-    // Configure TB0 period-timer
-    TB0CCTL0 = CCIE;                                    // CCR0 interrupt enabled
-    TB0CTL = TBSSEL_1 | TBCLR;                          // ACLK, clear TBR
+void configureGPIO(void) {
+    P3DIR |= BIT0;                                      // Set P3.0 output direction
+    P3SEL0 |= BIT0;                                     // Select Timer output for P3.0
+}
 
-    // Configure ADC timer trigger TB1.1
-    TB1CCR0 = 16384-1;                                  // PWM Period
-    TB1CCR1 = 8192-1;                                   // Duty cycle TB1.1
-    TB1CCTL1 = OUTMOD_3;                                // TB1CCR1 set/reset mode
-    TB1CTL = TBSSEL_1 | MC_1 | TBCLR;                   // ACLK, up mode
+int main(void) {
+    WDTCTL = WDTPW | WDTHOLD;               // Stop WDT
+
+    configureGPIO();
+    configureClocks();
+    configureReference();
+    configurePWM();
+    configureADC();
+    configureTimer1();
+
     ADCCTL0 |= ADCENC;                                  // Enable conversion
 
     __bis_SR_register(LPM3_bits | GIE);                 // Enter LPM3 w/ interrupts
@@ -138,62 +78,49 @@ int main(void)
 #pragma vector=ADC_VECTOR
 __interrupt void ADC_ISR(void)
 #elif defined(__GNUC__)
-void __attribute__ ((interrupt(ADC_VECTOR))) ADC_ISR (void)
+void __attribute__((interrupt(ADC_VECTOR))) ADC_ISR(void)
 #else
 #error Compiler not supported!
 #endif
 {
-    switch(__even_in_range(ADCIV, ADCIV_ADCIFG))
-    {
-        case ADCIV_NONE:
-            break;
-        case ADCIV_ADCOVIFG:
-            break;
-        case ADCIV_ADCTOVIFG:
-            break;
-        case ADCIV_ADCHIIFG:                            // ADCHI; A2 > 2.4V
-            if (low_threshold_crossed) {                // Check if low threshold was crossed
-                ADCIFG &= ~ADCHIIFG;                    // Clear interrupt flag
-                TB0CTL &= ~MC_1;                        // Turn off Timer
-                P1OUT &= ~BIT0;                         // Turn off LED on P1.0
-                low_threshold_crossed = 0;              // Reset the state
-                high_threshold_crossed = 1;             // Set the high threshold state
-            }
-            break;
-        case ADCIV_ADCLOIFG:                            // ADCLO; A2 < 1.7V
-            ADCIFG &= ~ADCLOIFG;                        // Clear interrupt flag
-            if (!high_threshold_crossed) {              // Only start blinking if high threshold not crossed
-                TB0CTL &= ~MC_1;                        // Turn off Timer
-                TB0CCR0 = SlowToggle_Period;            // Set Timer Period for slow LED toggle
-                TB0CTL |= MC_1;                         // Start Timer
-                low_threshold_crossed = 1;              // Set the low threshold state
-            } else {
-                low_threshold_crossed = 0;              // Reset the state if high threshold already crossed
-            }
-            break;
-        case ADCIV_ADCINIFG:                            // ADCIN; 1.7V < A2 < 2.4V
-            ADCIFG &= ~ADCINIFG;                        // Clear interrupt flag
-            TB0CTL &= ~MC_1;                            // Turn off Timer
-            P1OUT &= ~BIT0;                             // Turn off LED on P1.0
-            low_threshold_crossed = 0;                  // Reset the states
-            high_threshold_crossed = 0;
-            break;
-        case ADCIV_ADCIFG:
-            break;
-        default:
-            break;
+    if (ADCIV == ADCIV_ADCOVIFG) {
+        // ADC overflow
+    }
+    else if (ADCIV == ADCIV_ADCTOVIFG) {
+        // ADC conversion time overflow
+    }
+    else if (ADCIV == ADCIV_ADCHIIFG) {                // ADCHI; A2 > 2.4V
+        ADCIFG &= ~ADCHIIFG;                           // Clear interrupt flag
+        pwm_direction = 1;                             // Start increasing PWM duty cycle
+    }
+    else if (ADCIV == ADCIV_ADCLOIFG) {                // ADCLO; A2 < 1.7V
+        ADCIFG &= ~ADCLOIFG;                           // Clear interrupt flag
+        pwm_direction = -1;                            // Start decreasing PWM duty cycle
+    }
+    else if (ADCIV == ADCIV_ADCINIFG) {                // ADCIN; 1.7V < A2 < 2.4V
+        ADCIFG &= ~ADCINIFG;                           // Clear interrupt flag
+        pwm_direction = 0;                             // Stop changing PWM duty cycle
     }
 }
 
-// Timer0 B0 interrupt service routine
+// Timer1 B0 interrupt service routine
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
-#pragma vector=TIMER0_B0_VECTOR
-__interrupt void TIMER0_B0_ISR(void)
+#pragma vector=TIMER1_B0_VECTOR
+__interrupt void TIMER1_B0_ISR(void)
 #elif defined(__GNUC__)
-void __attribute__ ((interrupt(TIMER0_B0_VECTOR))) TIMER0_B0_ISR (void)
+void __attribute__((interrupt(TIMER1_B0_VECTOR))) TIMER1_B0_ISR(void)
 #else
 #error Compiler not supported!
 #endif
 {
-    P1OUT ^= BIT0;                                        // Toggle LED P1.0
+    if (pwm_direction != 0) {
+        pwm_duty_cycle += pwm_direction * 10;          // Adjust the duty cycle
+        if (pwm_duty_cycle > 1023) {                   // Clamp the duty cycle within 0-1023
+            pwm_duty_cycle = 1023;
+        }
+        else if (pwm_duty_cycle < 0) {
+            pwm_duty_cycle = 0;
+        }
+        TB0CCR1 = pwm_duty_cycle;                      // Update PWM duty cycle
+    }
 }
