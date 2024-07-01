64f296a4f951caffadcd6fc486bb65d4244fba16
Lennart Frank <lennart.frank@student.reutlingen-university.de>
2024-07-01 15:23:07 +0000

    funktionierender Pulsmesser rote LED leuchtet synchron blaue LED Asynchron

--- esr2024_g05_msp430pulseconverter/msp430fr235x_adc12_21.c
+++ esr2024_g05_msp430pulseconverter/msp430fr235x_adc12_21.c
@@ -1,72 +1,65 @@
 #include <msp430.h>
-
-#define THRESHOLD_VOLTAGE 3000  // ~2V (2486/4095 * 3.3V reference = 2V)
-
+ 
+#define THRESHOLD_VOLTAGE   3000  // ~2V (3000/4095 * 3.3V reference = 2V)
+ 
 void configureGPIO(void) {
-    // Configure LED (P3.0) as output
+    // Configure Red LED (P3.0) as output
     P3DIR |= BIT0;     // Set P3.0 output direction
     P3OUT &= ~BIT0;    // Initialize P3.0 to off
-
-    // Configure LED (P3.3) as output
-    P3DIR |= BIT3;     // Set P3.3 output direction
-    P3OUT |= BIT3;     // Turn on LED on P3.3
-
+ 
+    // Configure Blue LED (P3.2) as output
+    P3DIR |= BIT2;     // Set P3.2 output direction
+    P3OUT &= ~BIT2;    // Initialize P3.2 to off
+ 
     // Configure ADC input (P1.2)
     P1SEL0 |= BIT2;    // Set P1.2 for ADC function
     P1SEL1 |= BIT2;
 }
-
+ 
 void configureADC(void) {
     // Configure ADC
-    ADCCTL0 = ADCSHT_2 | ADCON;                     // Sample-and-hold time, ADC on
-    ADCCTL1 = ADCSHP;                               // ADC sample-and-hold pulse mode
-    ADCCTL2 = ADCRES_2;                             // 12-bit conversion results
-    ADCMCTL0 = ADCINCH_2 | ADCSREF_0;               // A2 input channel, Vcc/Vss reference
+    ADCCTL0 = ADCSHT_2 | ADCON;             // Sample-and-hold time, ADC on
+    ADCCTL1 = ADCSHP;                       // ADC sample-and-hold pulse mode
+    ADCCTL2 = ADCRES_2;                     // 12-bit conversion results
+    ADCMCTL0 = ADCINCH_2 | ADCSREF_0;       // A2 input channel, Vcc/Vss reference
 }
-
-void configureTimerB(void) {
-    // Configure Timer B
-    TB0CCTL0 = CCIE;                               // TBCCR0 interrupt enabled
-    TB0CCR0 = 16384 - 1;                           // 16384/32768 = 0.5s, assuming ACLK = 32.768 kHz
-    TB0CTL = TBSSEL__ACLK | MC__UP | TBCLR;        // ACLK, up mode, clear TAR
+ 
+void delay(unsigned int ms) {
+    // Assuming 1 MHz clock, 1 ms delay needs 1000 cycles
+    // 1 MHz / 1000 = 1 ms
+    while (ms--) {
+        __delay_cycles(1000); // 1 ms delay
+    }
 }
-
-#pragma vector = TIMER0_B0_VECTOR
-__interrupt void TIMER0_B0_ISR(void) {
-    // No action needed in ISR for LED toggling
-}
-
+ 
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;                       // Stop WDT
-
+    WDTCTL = WDTPW | WDTHOLD; // Stop WDT
+ 
     configureGPIO();
     configureADC();
-    configureTimerB();
-
+ 
     // Disable the GPIO power-on default high-impedance mode
     PM5CTL0 &= ~LOCKLPM5;
-
-    // Enable global interrupts
-    __bis_SR_register(GIE);
-
-    int thresholdCrossed = 0;
-
+ 
     while (1) {
-        ADCCTL0 |= ADCENC | ADCSC;                  // Enable and start conversion
-        while (ADCCTL1 & ADCBUSY);                  // Wait for conversion to complete
-
-        if (ADCMEM0 >= THRESHOLD_VOLTAGE) {         // Check if input signal is above 2V
-            if (!thresholdCrossed) {
-                P3OUT |= BIT0;                      // Turn on LED on P3.0
-
-                thresholdCrossed = 1;               // Set the flag to indicate threshold was crossed
-            }
+        // Start ADC conversion
+        ADCCTL0 |= ADCENC | ADCSC;
+ 
+        // Wait for ADC conversion to complete
+        while (ADCCTL1 & ADCBUSY);
+ 
+        // Check if ADC value is above the threshold
+        if (ADCMEM0 >= THRESHOLD_VOLTAGE) {
+            // Blink the LEDs
+            P3OUT ^= BIT0;
+            P3OUT &= ~BIT2;
+            delay(200);
+ 
+ 
         } else {
-            thresholdCrossed = 0;                   // Reset the flag when the signal falls below the threshold
-
-            P3OUT &= ~BIT0;                         // Turn off LED on P3.0
+            // Ensure LEDs are off if below threshold
+            P3OUT ^= BIT2;
+            P3OUT &= ~BIT0;
         }
     }
-
-    return 0;
 }
